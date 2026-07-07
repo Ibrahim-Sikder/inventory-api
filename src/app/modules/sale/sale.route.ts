0495de7cc322be5cd3d41b47ceb50c02310a34ab
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2026-07-07 23:01:26 +0600

    rolebased-problem-solve

--- src/app/modules/sale/sale.route.ts
+++ src/app/modules/sale/sale.route.ts
@@ -3,26 +3,25 @@ import { SaleController } from './sale.controller';
 import { SaleValidation } from './sale.validation';
 import { validateRequest } from '../../middlewares/validateRequest';
 import { auth } from '../../middlewares/auth';
-import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
 router.post(
     '/',
-    auth(USER_ROLE.admin, USER_ROLE.manager, USER_ROLE.employee),
+    auth('admin', 'manager', 'employee'),
     validateRequest(SaleValidation.createSaleValidationSchema),
     SaleController.createSale,
 );
 
 router.get(
     '/',
-    auth(USER_ROLE.admin, USER_ROLE.manager),
+    auth('admin', 'employee', 'manager'),
     SaleController.getAllSales,
 );
 
 router.get(
     '/:id',
-    auth(USER_ROLE.admin, USER_ROLE.manager),
+    auth('admin', 'manager', 'employee'),
     SaleController.getSingleSale,
 );
 
